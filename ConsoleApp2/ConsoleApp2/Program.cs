0c470fe38f529a3c6db470c1413337d339405488
VeronikaRomanenko <Veronika.Romanenko333@gmail.com>
2019-10-26 22:40:32 +0300

    Commit1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadStart threadstart = new ThreadStart(Method);
+            Thread thread = new Thread(threadstart);
+            Thread.Sleep(new Random().Next(1000, 5000));
+
+            Console.WriteLine("!!!!!!!");
+
+            System.Diagnostics.Stopwatch swatch = new System.Diagnostics.Stopwatch();
+            swatch.Start();
+
+            thread.Start();
+            thread.Join();
+
+            swatch.Stop();
+            Console.WriteLine(swatch.Elapsed.Milliseconds);
+        }
+
+        static void Method()
+        {
+            Console.ReadKey();
+        }
+    }
+}
